13f79221a5eb6091bade743fb3c40d292070f902
Jonathan Thornton <thorntonj1@xavier.edu>
2018-10-17 09:42:17 -0400

    Started working on Problem 7 and 8. Not complete.

---
+++ Project Euler/Problem7.cpp
@@ -0,0 +1,22 @@
+/*Project Euler
+Problem 7: 10001st prime
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+int main ()
+{
+    long input,ans;
+    cout << "Please enter in your intended prime number: ";
+    cin >> input;
+    for(int i = 0; i < input; i++) //prime at each position
+    {       
+    }
+}
